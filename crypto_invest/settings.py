de38b9c3e31ecd6670f0677d4ab13afc6d70ac53
Chijioke Divine <108485884+ChijiokeDivine@users.noreply.github.com>
2023-11-12 19:22:46 +0100

    Update settings.py

--- crypto_invest/settings.py
+++ crypto_invest/settings.py
@@ -83,13 +83,15 @@ WSGI_APPLICATION = "crypto_invest.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mto9hyCHRZRVGdAR',
+        'HOST': 'db.fqjvatobreqdzaelsgxt.supabase.co',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 

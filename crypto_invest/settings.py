8d34c742da51e9345610eaef300a1b5aa9c5f1f7
Chijioke Divine <108485884+ChijiokeDivine@users.noreply.github.com>
2023-11-13 12:00:27 +0100

    Update settings.py

--- crypto_invest/settings.py
+++ crypto_invest/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0tgu5vhts8_)lhzrzazlq)sv0=dcmdq^z7-!sz8c-(o4!a4xu3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["bittrade.onrender.com"]
 
